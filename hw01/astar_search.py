6c593b7a85c2905ff820573a8b2ad502b1b002dc
Morgan Rockett <morgan.rockett@tufts.edu>
2022-06-12 19:08:11 -0400

    calc edge weights now

--- hw01/astar_search.py
+++ hw01/astar_search.py
@@ -27,7 +27,8 @@ def read_file(file):
 
     with open(file, 'r') as fi:
 
-        coords = False; distances = False
+        coords = False
+        distances = False
 
         for line in fi:
 
@@ -45,8 +46,8 @@ def read_file(file):
                 result = re.match(r'(.+)\s+(-*[\d.]+)\s+(-*[\d.]+)', line)
                 if result:
                     city = result.group(1)
-                    lat = result.group(2)
-                    long = result.group(3)
+                    lat = float(result.group(2))
+                    long = float(result.group(3))
                     # print(f'\tcity: {city}; lat: {lat}; long: {long}\n')
                     if city not in city_dict.keys():
                         city_dict[city] = [lat, long]
@@ -58,7 +59,7 @@ def read_file(file):
                 if result:
                     city1 = result.group(1)
                     city2 = result.group(2)
-                    dist = result.group(3)
+                    dist = float(result.group(3))
                     # print(f'\tcity1: {city1}; city2: {city2}; dist: {dist}\n')
                     if city1 not in dist_dict.keys():
                         dist_dict[city1] = []
@@ -71,10 +72,14 @@ def read_file(file):
 
 
 def create_graph(city_dict: dict, dist_dict: dict):
+
     graph = nx.Graph()
 
     for k, v in city_dict.items():
         graph.add_node(k)
+        graph.nodes[k]['pos'] = (v[0], v[1])  # city lat lon
+
+    # print(f"{graph}\n{graph.nodes}\n{graph.nodes['Winona']['pos'][0]}")
 
     for keys, values in dist_dict.items():
         for elem in values:
@@ -85,9 +90,44 @@ def create_graph(city_dict: dict, dist_dict: dict):
 
             graph.add_edge(city1, city2, length=dist)
 
-    # print(f'{G}\n{G.nodes}\n{G.edges}\n')
+    # print(f'{graph}\n{graph.nodes}\n{graph.edges}\n')
 
     return graph
+
+
+def calc_direct_distance(graph, S, G, debug=False):
+
+    lon2 = graph.nodes[G]['pos'][1]
+    lon1 = graph.nodes[S]['pos'][1]
+
+    lat2 = graph.nodes[G]['pos'][0]
+    lat1 = graph.nodes[S]['pos'][0]
+
+    lon1, lon2, lat1, lat2 = map(radians, [lon1, lon2, lat1, lat2])  # convert from degrees to radians
+
+    lon_dist = lon2 - lon1
+    lat_dist = lat2 - lat1
+
+    a = pow(sin(lat_dist/2), 2) + cos(lat1) * cos(lat2) * pow(sin(lon_dist/2), 2)
+
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+    d_total = round(c * 3958.8, 2)  # radius of the earth in miles (6371 km)
+
+    if debug:
+        print(f"Distance between starting city: '{S}' and goal city: '{G}' is {d_total} miles.")
+    return d_total
+
+
+def update_edge_weights(graph, debug=False):
+
+    for edge in graph.edges:
+
+        dist = calc_direct_distance(graph, S=edge[0], G=edge[1])
+
+        graph.edges[edge]['length'] = dist
+
+        if debug:
+            print(f'edge: {edge}: graph.edges[edge]: {graph.edges[edge]}')
 
 
 def input_handling(graph, src_or_dst, var): # input_handling(graph, 'Starting', 'S')
@@ -118,27 +158,6 @@ def prompt_user(graph):  # 'S' = starting city; 'G' = goal city
     return S, G
 
 
-def astar_search(S, G, graph, city_dict):
-
-    lon2 = float(city_dict[G][1])
-    lon1 = float(city_dict[S][1])
-
-    lat2 = float(city_dict[G][0])
-    lat1 = float(city_dict[S][0])
-
-    lon1, lon2, lat1, lat2 = map(radians, [lon1, lon2, lat1, lat2])  # convert from degrees to radians
-
-    lon_dist = lon2 - lon1
-    lat_dist = lat2 - lat1
-
-    a = pow(sin(lat_dist/2), 2) + cos(lat1) * cos(lat2) * pow(sin(lon_dist/2), 2)
-
-    c = 2 * atan2(sqrt(a), sqrt(1-a))
-    d_total = c * 3958.8  # radius of the earth in miles (6371 km)
-
-    print(f"Distance between starting city: '{S}' and goal city: '{G}' is {d_total} miles.")
-
-
 if __name__ == "__main__":
 
     file = parse_file()
@@ -147,11 +166,8 @@ if __name__ == "__main__":
 
     graph = create_graph(city_dict, dist_dict)
 
+    update_edge_weights(graph, debug=True)
+
     S, G = prompt_user(graph)
 
-    astar_search(S, G, graph, city_dict)
-
-
-# References
-#     http://andrew.hedges.name/experiments/haversine/
-#     https://medium.com/@petehouston/calculate-distance-of-two-locations-on-earth-using-python-1501b1944d97
+    calc_direct_distance(graph, S, G, debug=True)
