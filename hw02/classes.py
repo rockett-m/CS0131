b2cb863992e5ac32e1d5f90523512407f5a9bcc2
Morgan Rockett <morgan.rockett@tufts.edu>
2022-07-04 21:20:29 -0400

    solver work

--- hw02/classes.py
+++ hw02/classes.py
@@ -15,6 +15,12 @@ class Variable():
         self.length =    length
         self.direction = direction.upper()
         self.name =      name
+        self.domain =    []
+        self.cells =     []
+
+        for k in range(self.length):
+            self.cells.append((self.row + (k if self.direction == Variable.DOWN else 0),
+                               self.col + (k if self.direction == Variable.ACROSS else 0)))
 
     def __hash__(self):  # dictionary
         return hash((self.row, self.col, self.length, self.direction, self.name))
@@ -48,44 +54,29 @@ class Crossword():
 
         with open(xword_file, 'r') as fi:
 
-            grid_words = np.zeros((0, 0))
             ccount = 0; lcount = -1
 
             for line in fi:
                 if lcount == -1:  # zeros grid to match input dimensions
                     result = re.match(r'\s*(\d+)\s+(\d+)\s*', line)
-                    if result:
-                        self.width = int(result.group(1)); self.height = int(result.group(2))
-                    grid_words = np.zeros((self.width, self.height))  # populate with 1 if char belongs
-                    self.grid_numbers = np.zeros((self.width, self.height))  # populate with 1 if char belongs
-                    self.grid_numbers = self.grid_numbers.astype('int')
+                    if result: self.width = int(result.group(1)); self.height = int(result.group(2))
+                    self.grid_numbers = np.zeros((self.width, self.height)).astype('int')  # populate with 1 if char belongs
 
                 else:  # add 1's to grid x,y coords if letter belongs there
                     line = line.strip(); fields = line.split(' '); new_fields = []
                     for i in fields:
-                        if i != '':
-                            new_fields.append(i)
+                        if i != '': new_fields.append(i)
 
                     for char in new_fields:  # build word list horizontal of words in a line
                         if re.search(r'\d+', char):  # word begin
-                            # print(f'line: {lcount}; char: {ccount}; char: >{char}<; line: {line};')
-                            grid_words[lcount, ccount] = 2
                             self.grid_numbers[lcount, ccount] = int(char)
-                            ccount += 1
                         elif re.search('_', char):
-                            # print(f'line: {lcount}; char: {ccount}; char: >{char}<; line: {line};')
-                            grid_words[lcount, ccount] = 1
                             self.grid_numbers[lcount, ccount] = 0
-                            ccount += 1
                         elif re.search(r'X', char):  # end and reset counts
                             self.grid_numbers[lcount, ccount] = -1
-                            ccount += 1
+                        ccount += 1
+
                 ccount = 0; lcount += 1
-
-
-        print(f'{self.grid_numbers=}')
-
-        self.variables = set()
 
         """
         # ==  #
@@ -93,84 +84,125 @@ class Crossword():
         _ ==  0
         """
 
-        store_words = []
+        self.variables = set()
         # store_words = [[row, col, length, direction='Across'|'Down'],[row, col, length, direction='Across'|'Down']]
-
+        store_words = []
         for row in range(self.height):
             word_index = []; name = ''
 
             for col_idx in range(self.width):
-
                 char = self.grid_numbers[row][col_idx]
 
                 if (char > 0) and ((col_idx == 0) or (len(word_index) == 0)):
                     name = f'{char}-ACROSS'
-                    word_index = [row, col_idx, 1, f'{char}-ACROSS']
+                    word_index = [row, col_idx, 1, name]
 
                 elif (char >= 0) and (len(word_index) > 0):  # add length   if _ or #
                     word_index[2] += 1
                     if col_idx == (self.width - 1):
                         store_words.append(word_index)
-                        self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='ACROSS', name=name))
-                        word_index = []
-
-                elif (char == -1) and (col_idx != self.width - 1) and (len(word_index) > 0):  # stop - end
-                    store_words.append(word_index)
-                    self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='ACROSS', name=name))
-                    word_index = []
-
-                elif (char == -1) and (col_idx == self.width - 1) and (len(word_index) > 0):  # stop - end
-                    store_words.append(word_index)
-                    self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='ACROSS', name=name))
-                    word_index = []
-
-                elif (char == -1) and (len(word_index) == 0):  # stop - end
-                    word_index = []
-
-        for i in store_words:
-            print(i)
-
-        print(f'{len(store_words)}')
-
-
-        store_words = []
-
-        for row in range(self.height):
-            word_index = []; name = ''
-
-            for col_idx in range(self.width):
-
-                char = self.grid_numbers[col_idx][row]
-
-                if (char > 0) and ((col_idx == 0) or (len(word_index) == 0)):
-                    name = f'{char}-DOWN'
-                    word_index = [col_idx, row, 1, f'{char}-DOWN']
-
-                elif (char >= 0) and (len(word_index) > 0):  # add length   if _ or #
-                    word_index[2] += 1
-                    if col_idx == (self.width - 1):
-                        store_words.append(word_index)
+                        # domain = self.find_domain(length=word_index[2])
                         self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='DOWN', name=name))
                         word_index = []
 
                 elif (char == -1) and (col_idx != self.width - 1) and (len(word_index) > 0):  # stop - end
                     store_words.append(word_index)
+                    # domain = self.find_domain(length=word_index[2])
                     self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='DOWN', name=name))
                     word_index = []
 
                 elif (char == -1) and (col_idx == self.width - 1) and (len(word_index) > 0):  # stop - end
                     store_words.append(word_index)
+                    # domain = self.find_domain(length=word_index[2])
                     self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='DOWN', name=name))
-                    word_index = []
 
                 elif (char == -1) and (len(word_index) == 0):  # stop - end
                     word_index = []
 
-        for i in store_words:
-            print(i)
+        # for i in store_words: print(i)
+        self.horizontal_word_count = len(store_words)
 
-        print(f'{len(store_words)}')
+        store_words = []
+        for row in range(self.height):
+            word_index = []; name = ''
 
+            for col_idx in range(self.width):
+                char = self.grid_numbers[col_idx][row]
+
+                if (char > 0) and ((col_idx == 0) or (len(word_index) == 0)):
+                    name = f'{char}-DOWN'
+                    word_index = [col_idx, row, 1, name]
+
+                elif (char >= 0) and (len(word_index) > 0):  # add length   if _ or #
+                    word_index[2] += 1
+                    if col_idx == (self.width - 1):
+                        store_words.append(word_index)
+                        # domain = self.find_domain(length=word_index[2])
+                        self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='DOWN', name=name))
+                        word_index = []
+
+                elif (char == -1) and (col_idx != self.width - 1) and (len(word_index) > 0):  # stop - end
+                    store_words.append(word_index)
+                    # domain = self.find_domain(length=word_index[2])
+                    self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='DOWN', name=name))
+                    word_index = []
+
+                elif (char == -1) and (col_idx == self.width - 1) and (len(word_index) > 0):  # stop - end
+                    store_words.append(word_index)
+                    # domain = self.find_domain(length=word_index[2])
+                    self.variables.add(Variable(row=word_index[0], col=word_index[1], length=word_index[2], direction='DOWN', name=name))
+
+                elif (char == -1) and (len(word_index) == 0):  # stop - end
+                    word_index = []
+
+        # for i in store_words: print(i)
+        self.vertical_word_count = len(store_words)
+
+
+        ## DOMAIN ##
+        self.domain_dict = OrderedDict()
+
+        for i in self.variables:
+            domain = self.find_domain(i.length)
+            self.domain_dict[i.name] = domain
+
+        # for k, v in self.domain_dict.items():
+        #     print(f'{k=} {v=}')
+        ## DOMAIN ##
+
+        self.intersections = dict()
+        for var1 in self.variables:
+            for var2 in self.variables:
+                if var1 == var2:
+                    continue
+                cells1 = var1.cells
+                cells2 = var2.cells
+
+                intersection = set(cells1).intersection(cells2)
+                if not intersection:
+                    self.intersections[var1, var2] = None
+                else:
+                    intersection = intersection.pop()
+                    self.intersections[var1, var2] = (cells1.index(intersection), cells2.index(intersection))
+
+    def find_domain(self, length):
+        domain = []
+        for word in self.words:
+            if len(word) == length:
+                domain.append(word)
+        return domain
+
+    def neighbors(self, var):
+        neighbors = []
+        for v in self.variables:
+            if (v != var) and (self.intersections[v, var]):
+                neighbors.append(v)
+        neighbors = set(neighbors)
+        return neighbors
+
+    def print_domains(self):
+        for i in self.variables:
+            print(f'{i=}')
 
     def print_input_files(self):
         print(f'\n{self.width} x {self.height} crossword detected\n')
@@ -183,23 +215,21 @@ class Crossword():
     def print_xword_and_words(self):
         print(f'\n{self.words = }\n')
         print(f'\n{self.orig_xword = }\n')
-
-    def print_grids(self):
-        print(f'\nself.grid_horz:\n{self.grid_horz}\n')
-        print(f'\nself.grid_vert:\n{self.grid_vert}\n')
+        # print(f'{self.grid_numbers=}')
 
     def print_words_to_solve(self):
-        print(f'\nwords to solve: {len(self.variables)}\n{self.variables = }\n')
+        print(f'\nwords to solve: {len(self.variables) = }\n')
         # [ print(i) for i in self.variables ]
-        vert_words = 0; across_words = 0
+        # vert_words = 0; across_words = 0
+        #
+        # for var in self.variables:
+        #     if var.direction == 'DOWN': vert_words += 1
+        #     elif var.direction == 'ACROSS' : across_words += 1
+        #
+        # print(f'\n{vert_words = }\n'); print(f'\n{across_words = }\n')
 
-        for var in self.variables:
-            if var.direction == 'DOWN': vert_words += 1
-            elif var.direction == 'ACROSS' : across_words += 1
-
-        print(f'\n{vert_words = }\n'); print(f'\n{across_words = }\n')
-
-
+        print(f'{self.vertical_word_count = }')
+        print(f'{self.horizontal_word_count = }')
 
 # word class / streak - start location, length, direction
 
--- hw02/cons_solver.py
+++ hw02/cons_solver.py
@@ -2,7 +2,6 @@
 import math
 import os
 import sys
-import re
 import time
 from collections import OrderedDict
 from functools import wraps
@@ -45,60 +44,59 @@ def parse_files():
     return xword_file, word_file
 
 
-def crossword_solver(word_dict, grid_horz):
 
-    # for word in word_dict.sorted():
-    row_idx = 0
-    for row in grid_horz:
+class Crossword_Solver():
+    def __init__(self, crossword):
+        self.crossword = crossword
+        # self.domains = {
+        #     var: self.crossword.words.copy()
+        #     for var in self.crossword.variables
+        # }
+        # self.domains = {
+        #     var: [ x for x in self.crossword.words if len(x) == var ]
+        #     # for var in self.crossword.variables
+        # }
+        #
+        # self.domains = {
+        #
+        # domain_list = []
+        # for var in self.crossword.variables:
+        #     for word in self.crossword.words:
+        #         if len(word) == var.length:
+        #             domain_list.append(word)
+        #             var: self.domains = domain_list
+        # }
 
-        col_idx = streak = start_col_idx = end_col_idx = prev_item = prev_col_idx = 0
-        for item in row:
-            item = int(item)
+    def assignment_complete(self, assignment):
+        if len(assignment) == len(self.crossword.variables):
+            return True
+        return False
 
-            if (item == 0) and (streak > 0):  # at least one letter position before here
-                end_col_idx = col_idx - 1  # prior col - last letter location
-                print(f'streak {streak} over: begin @ {row_idx, start_col_idx} end @ {row_idx, end_col_idx}')
+    def is_consistent(self, assignment):
+        if len(assignment) == 1:
+            return True
+        for var in assignment:
+            copy = assignment.copy()
+            copy.pop(var)
 
-            elif item > 0:  # start of word or blank
-                if streak > 0: streak += 1  # word has been detected already
-                # prev was border or an 'X' and current is a '#'
-                if (item == 2) and ((prev_item == 0) or (prev_col_idx == 0)):  # beginning of horz word - must start from boundary
-                    streak = 1; start_col_idx = col_idx
+            for var2 in copy:
+                if assignment[var] == assignment[var2]:
+                    return False
+                crossing = self.crossword.intersections((var, var2))
+                if crossing is not None:
+                    x = crossing[0]
+                    y = crossing[1]
+                    if assignment[var][x] != assignment[var2][y]:
+                        return False
 
-                if (col_idx == WIDTH-1) and (streak > 0):  # word over due to end of line
-                    end_col_idx = col_idx
-                    print(f'streak {streak} over: begin @ {row_idx, start_col_idx} end @ {row_idx, end_col_idx}')
-
-            prev_item = item; prev_col_idx = col_idx
-
-            col_idx += 1
-        row_idx += 1
-
-
-
-
-    # for recursive, call backtrack from inside backtrack
-
-    # assignment aka solution says what each word should be - to each var
-
-    # every time backtrack is called, partial solution is called
-
-    # on outer loop, the entire solution is returned
-
-
-
-    # each var is say 1-down
-
-    # for each value in domain list - has to be same number of letters as blank
-
-    # violations - intersections have to work ok
-
-
-    # keep class for keeping track of variable
-
-
-
-    # do the same thing here
+        return True
+# def domain_updater(word_file):
+#
+#     words = open(word_file, 'r').read().splitlines()
+#
+#     for
+#
+#     domain = []
 
 
 def solve_crossword(xword_file, word_file):
@@ -110,7 +108,12 @@ def solve_crossword(xword_file, word_file):
     # crossword.print_xword_and_words()
     crossword.print_words_to_solve()
 
+    # crossword.print_domains()
+
+    crossword_solver = Crossword_Solver(crossword)
+    # print(f'{crossword_solver.domains=}')
     # crossword.find_word_locations()
+
 
 if __name__ == "__main__":
 
@@ -121,7 +124,30 @@ if __name__ == "__main__":
     sys.exit()
 
 
-    crossword_solver(word_dict, grid_horz=grid_words)
+# for recursive, call backtrack from inside backtrack
+
+# assignment aka solution says what each word should be - to each var
+
+# every time backtrack is called, partial solution is called
+
+# on outer loop, the entire solution is returned
+
+
+
+# each var is say 1-down
+
+# for each value in domain list - has to be same number of letters as blank
+
+# violations - intersections have to work ok
+
+
+# keep class for keeping track of variable
+
+
+
+# do the same thing here
+
+
 
 
 
@@ -129,32 +155,23 @@ if __name__ == "__main__":
 """
 ./cons_solver.py ./inputData/xword00.txt ./inputData/dictionary_small.txt
 
- 7 7
- 1  _  _  _  _  _  2
- _  X  X  X  X  X  _
- _  X  X  X  X  X  _
- _  X  X  X  X  X  _
- _  X  X  X  X  X  _
- _  X  X  X  X  X  _
- 3  _  _  _  _  _  _
 
-grid_words : 
 
-[[2. 1. 1. 1. 1. 1. 2.]
- [1. 0. 0. 0. 0. 0. 1.]
- [1. 0. 0. 0. 0. 0. 1.]
- [1. 0. 0. 0. 0. 0. 1.]
- [1. 0. 0. 0. 0. 0. 1.]
- [1. 0. 0. 0. 0. 0. 1.]
- [2. 1. 1. 1. 1. 1. 1.]]
- 
- grid_words_vert : 
+15 15
+ 1  2  3  4  X  5  6  7  8  X  X  9 10 11 12
+13  _  _  _  X 14  _  _  _ 15  X 16  _  _  _
+17  _  _  _ 18  _  _  _  _  _ 19  _  _  _  _
+20  _  _  _  _  _  X 21  _  _  _  X 22  _  _
+ X  X  X  X 23  _ 24  _  _  X 25 26  _  _  _
+27 28 29 30  _  _  _  _  _  X 31  _  _  X  X
+32  _  _  _  X 33  _  _  X 34  _  _  _ 35  X
+36  _  _  _ 37  _  _  X 38  _  _  _  _  _ 39
+ X 40  _  _  _  _  X 41  _  _  X 42  _  _  _
+ X  X 43  _  _  X 44  _  _  _ 45  _  _  _  _
+46 47  _  _  _  X 48  _  _  _  _  X  X  X  X
+49  _  _  X 50 51  _  _  X 52  _ 53 54 55 56
+57  _  _ 58  _  _  _  _ 59  _  _  _  _  _  _
+60  _  _  _  X 61  _  _  _  _  X 62  _  _  _
+63  _  _  _  X  X 64  _  _  _  X 65  _  _  _
 
- [[2. 1. 1. 1. 1. 1. 2.]
-  [1. 0. 0. 0. 0. 0. 1.]
-  [1. 0. 0. 0. 0. 0. 1.]
-  [1. 0. 0. 0. 0. 0. 1.]
-  [1. 0. 0. 0. 0. 0. 1.]
-  [1. 0. 0. 0. 0. 0. 1.]
-  [2. 1. 1. 1. 1. 1. 1.]]
 """
